cccd0d6c32a179de13180cf14d4e2a2404cb760d
Ivan <ivanka322@gmail>
2023-01-25 16:34:30 +0300

    Example005_Condition_If_Else

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower()  == "иван")
+{
+    Console.WriteLine("Ура, это же Иван!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
